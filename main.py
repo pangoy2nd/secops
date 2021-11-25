54d401bd1e614fdf7dff82f5ce1b1db337359e21
pangoy2nd <pangoy2nd@yahoo.com>
2021-11-25 16:24:17 +0800

    Im useful

--- main.py
+++ main.py
@@ -6,8 +6,8 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    print('Im quite useful! :)')
+    print(f'Hi, {name}, great things is about to start...')  # Press ⌘F8 to toggle the breakpoint.
+
 
 
 # Press the green button in the gutter to run the script.
